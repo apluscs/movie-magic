0ef314484a64deaa7491f31052b5aa558965595c
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-24 16:22:58 -0400

    s

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class SiteUser(ndb.Model):
 class MainPage(webapp2.RequestHandler): #inheritance
     def get(self):
         user=users.get_current_user()
-        print(user)
+        # print(user)
         logout_url=users.create_logout_url('/')
         index_template=jinjaEnv.get_template('index.html')
         index_dict={}
@@ -27,7 +27,7 @@ class MainPage(webapp2.RequestHandler): #inheritance
             index_dict={
                 "hideLogOut": "hidden=\"\" "
             }
-        print(index_dict)
+        # print(index_dict)
         self.response.write(index_template.render(index_dict))
 
 class LoginPage(webapp2.RequestHandler):
@@ -41,13 +41,13 @@ class LoginPage(webapp2.RequestHandler):
             self.redirect("/register")   #send to register
     def get(self):
         user=users.get_current_user()
-        print(user)
+        # print(user)
         logout_url=users.create_logout_url('/') #redirect to this link
         if user:    #someone is already logged in to gmail
             self.checkExistingUser(logout_url)
         else:   #not a google user
             login_url=users.create_login_url('/login')
-            print(login_url)
+            # print(login_url)
             login_dict={
                 "login_url": login_url
             }
@@ -81,6 +81,7 @@ class ShowsResultPage(webapp2.RequestHandler):  #add a theatre radius parameter
 class MovieResultPage(webapp2.RequestHandler):
     def post(self):
         user=users.get_current_user()
+
         movie_title=self.request.get('movie_title')
         zip_code=""
         # print(movie_title)
@@ -93,6 +94,7 @@ class MovieResultPage(webapp2.RequestHandler):
             self.redirect("/login")  #send to login to register
             return
         zip_code="48098"#existing_user.zip_code, will replace after validation step
+
         date=datetime.datetime.now().strftime("%Y-%m-%d")
         api_url="http://data.tmsapi.com/v1.1/movies/showings?startDate=%s&zip=%s&api_key=f5ty9m8fjg5hbwby658ccc75" % (date, zip_code)
         gracenote_response_json = urlfetch.fetch(api_url).content
@@ -109,7 +111,9 @@ class MovieResultPage(webapp2.RequestHandler):
         movie_result_template=jinjaEnv.get_template('movie-result.html')
         self.response.write(movie_result_template.render(movie_result_dict))
 
-
+class GetLocationPage(webapp2.RequestHandler):
+    def get(self):
+        pass
 
 class ResultsPage(webapp2.RequestHandler):
     def get(self):
@@ -122,7 +126,7 @@ class ResultsPage(webapp2.RequestHandler):
         tastedive_response_json = urlfetch.fetch(api_url).content
         tastedive_response_raw = json.loads(tastedive_response_json)
         recommendationList = []
-        print(recommendationList)
+        # print(recommendationList)
         for results in tastedive_response_raw['Similar']['Results'][0:50]:
             recommendationList.append(results["Name"])
         titleAndPic = {}
@@ -141,7 +145,7 @@ class ResultsPage(webapp2.RequestHandler):
                     urls.append(link)
                     titleAndPic[item] = link
         OMDBurl = "http://www.omdbapi.com/?t=" + q + "&apikey=" + OMDBkey
-        print(OMDBurl)
+        # print(OMDBurl)
         OMDB_response_json = urlfetch.fetch(OMDBurl).content
         OMDB_response_raw = json.loads(OMDB_response_json)
         # print(OMDB_response_raw)
@@ -166,7 +170,8 @@ app=webapp2.WSGIApplication(
         ('/results', ResultsPage),
         ('/register',RegisterPage),
         ('/movie-results',MovieResultPage),
-        ('/shows-results',ShowsResultPage)
+        ('/shows-results',ShowsResultPage),
+        ('/get-location',GetLocationPage)
     ],
     debug=True    #parameter 1
 )
