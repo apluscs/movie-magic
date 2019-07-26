a117a7c498c4eb9eabf676c53e62f3e6c701399e
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-26 10:35:29 -0400

    s

--- main.py
+++ main.py
@@ -101,11 +101,11 @@ class MovieResultPage(webapp2.RequestHandler):
                 break   #showed_movies should contain only 0 or 1 movies, but just in case it finds 2
         showtime_dict=self.groupByTheatre(showed_movie)
         movie_result_dict={
-        #     "showtime_dict":showtime_dict,
-        #     "selected_movie": movie_title
+            "showtime_dict":showtime_dict,
+            "selected_movie": movie_title
         }
         checkLogIn(movie_result_dict)
-
+        print(showtime_dict)
         movie_result_template=jinjaEnv.get_template('movie-result.html')
         self.response.write(movie_result_template.render(movie_result_dict))
     def groupByTheatre(self,showed_movie):   #return array of Theatres
